77a6700e3ed96c21ee9b260337f0a14ac999b411
qiangzengy <qiangzengy@163.com>
2020-04-30 15:24:22 +0800

    多级缓存项目搭建

---
+++ src/main/java/com/qiangzengy/eshop/cache/EshopCacheApplication.java
@@ -0,0 +1,15 @@
+package com.qiangzengy.eshop.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class EshopCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qiangzengy/eshop/cache/EshopCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiangzengy.eshop.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
